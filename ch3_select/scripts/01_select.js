d5aed92d28d3005e519ec10cc114fca3cd4feb7e
chenxlong3 <1226608021@qq.com>
2022-01-12 23:38:55 +0800

    paragraph eg

--- ch3_select/scripts/01_select.js
+++ ch3_select/scripts/01_select.js
@@ -4,5 +4,14 @@ const firstP = d3.select("p");
 const allParagraphs = d3.selectAll("p");
 const allPDom = allParagraphs.nodes();
 const sectionParagraphs = d3.select("div").selectAll("p");
-console.log(allPDom);
-console.log(sectionParagraphs);
+// console.log(allPDom);
+// console.log(sectionParagraphs);
+div.style("border", "solid blue 2px");
+firstP.classed("big", true).text("This is paragraph");
+
+allParagraphs.style("font-weight", "bold");
+
+d3.select(".big")
+    .attr("title", "Tooltip")
+    .style("font-variant", "small-caps")
+    .html("This is a <b>bold</b> paragraph.");
