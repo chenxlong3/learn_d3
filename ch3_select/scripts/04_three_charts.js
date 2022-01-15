f58e2905abfb631c5abf7e8f7880dc40ed26a4d5
chenxlong3 <1226608021@qq.com>
2022-01-15 22:35:10 +0800

    add circle

--- ch3_select/scripts/04_three_charts.js
+++ ch3_select/scripts/04_three_charts.js
@@ -1,7 +1,7 @@
 const charts = [
-    {key: "avg", title: "Average", color: "orange"},
-    {key: "max", title: "Maximum", color: "blue"},
-    {key: "min", title: "Minimum", color: "red"}
+    { key: "avg", title: "Average", color: "orange" },
+    { key: "max", title: "Maximum", color: "blue" },
+    { key: "min", title: "Minimum", color: "red" }
 ]
 
 const chart = {
@@ -16,13 +16,14 @@ const format = d3.format(".2f");
 
 const svg = d3.select("svg.bar-chart");
 const planets = [];
-d3.json("../Data/sol_2016.json").then(function(data) {
-    data.planets.forEach(function(obj) {
+d3.json("../Data/sol_2016.json").then(function (data) {
+    data.planets.forEach(function (obj) {
         planets.push({
             name: obj.name,
             avg: obj.semiMajorAxisAU,
             max: obj.apheliumAU,
-            min: obj.periheliumAU});
+            min: obj.periheliumAU
+        });
     });
     init();
 });
@@ -30,7 +31,7 @@ d3.json("../Data/sol_2016.json").then(function(data) {
 function setupView() {
     d3.selectAll("button").property("disabled", false);
     d3.select("#" + chart.current.key).property("disabled", true);
-    planets.sort((a,b) => d3.ascending(a[chart.current.key], b[chart.current.key]));
+    planets.sort((a, b) => d3.ascending(a[chart.current.key], b[chart.current.key]));
 
     const maxValue = d3.max(planets, d => d[chart.current.key]);
     barScale.domain([0, maxValue]);
@@ -40,64 +41,64 @@ function setupView() {
 function init() {
     chart.height = planets.length * 35;
     svg.attr("width", chart.width)
-    .attr("height", chart.height);
+        .attr("height", chart.height);
 
     setupView();
     svg.selectAll("g").data(planets)
-    .enter()
-    .append("g")
-    .attr("class", "entry")
-    .attr("transform", (d, i) => `translate(0, ${i * 35})`)
-    .each(function(d) {
-        const entry = d3.select(this);
-        entry.append("text")
-        .attr("class", "label category")
-        .attr("y", 15)
-        .attr("x", 50)
-        .text(d.name);
+        .enter()
+        .append("g")
+        .attr("class", "entry")
+        .attr("transform", (d, i) => `translate(0, ${i * 35})`)
+        .each(function (d) {
+            const entry = d3.select(this);
+            entry.append("text")
+                .attr("class", "label category")
+                .attr("y", 15)
+                .attr("x", 10)
+                .text(d.name);
 
-        entry.append("rect")
-        .attr("class", "bar")
-        .attr("x", 100)
-        .attr("height", 33)
-        .attr("width", barScale(d[chart.current.key]))
-        .style("fill", d3.color(chart.current.color)
-        .darker(colorScale(d[chart.current.key])));
+            entry.append("rect")
+                .attr("class", "bar")
+                .attr("x", 100)
+                .attr("height", 33)
+                .attr("width", barScale(d[chart.current.key]))
+                .style("fill", d3.color(chart.current.color)
+                    .darker(colorScale(d[chart.current.key])));
 
-        entry.append("text")
-        .attr("class", "label value")
-        .attr("x", barScale(d[chart.current.key]) + 105)
-        .attr("y", 15)
-        .text(format(d[chart.current.key]) + " AU");
-        
-    });
+            entry.append("text")
+                .attr("class", "label value")
+                .attr("x", barScale(d[chart.current.key]) + 105)
+                .attr("y", 15)
+                .text(format(d[chart.current.key]) + " AU");
+
+        });
     d3.selectAll("button")
-    .on("click", function() {
-        chart.current = charts.filter(c => c.key == this.id)[0];
-        d3.select("#chart")
-        .text(chart.current.title)
-        draw();
-    })
+        .on("click", function () {
+            chart.current = charts.filter(c => c.key == this.id)[0];
+            d3.select("#chart")
+                .text(chart.current.title)
+            draw();
+        })
 }
 
 function draw() {
     // console.log(chart.current.key);
     setupView();
     svg.selectAll("g.entry")
-    .data(planets)
-    .each(function (d, i) {
-        d3.select(this).select(".label.category")
-        .text(d.name);
-        d3.select(this).select(".bar")
-        .transition()
-        .delay(20 * i)
-        .attr("width", barScale(d[chart.current.key]))
-        .style("fill", d3.color(chart.current.color).darker(colorScale(d[chart.current.key])));
+        .data(planets)
+        .each(function (d, i) {
+            d3.select(this).select(".label.category")
+                .text(d.name);
+            d3.select(this).select(".bar")
+                .transition()
+                .delay(20 * i)
+                .attr("width", barScale(d[chart.current.key]))
+                .style("fill", d3.color(chart.current.color).darker(colorScale(d[chart.current.key])));
 
-        d3.select(this).select(".label.value")
-        .transition()
-        .delay(20 * i)
-        .attr("x", barScale(d[chart.current.key]) + 105)
-        .text(format(d[chart.current.key]) + " AU");
-    })
+            d3.select(this).select(".label.value")
+                .transition()
+                .delay(20 * i)
+                .attr("x", barScale(d[chart.current.key]) + 105)
+                .text(format(d[chart.current.key]) + " AU");
+        })
 }
